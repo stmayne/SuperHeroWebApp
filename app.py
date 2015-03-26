990f7d3b8b42d3fefa98e6f14eb71a9b4b2e3bd0
Joe Van Loon <joevanloon@utexas.edu>
2015-03-26 13:07:31 -0500

    Added json data
    Also hooked it into templating.

--- app.py
+++ app.py
@@ -5,9 +5,12 @@ import traceback
 
 from config import app
 
-from models import Characters
-from models import Comics
-from models import TvShows
+import models
+
+import json
+
+with open('Data.json') as json_data:
+    data = json.load(json_data)
 
 @app.route('/')
 def get():
@@ -19,15 +22,15 @@ def getDirectory():
 
 @app.route('/comic/<comic_id>')
 def getComic(comic_id):
-    return render_template('comic.html', comic_id=comic_id)
+    return render_template('comic.html', **data['Comics'][comic_id])
 
 @app.route('/show/<show_id>')
 def getShow(show_id):
-    return render_template('show.html', show_id=show_id)
+    return render_template('show.html', **data['Shows'][show_id])
 
 @app.route('/character/<character_id>')
 def getCharacter(character_id):
-    return render_template('character.html', character_id=character_id)
+    return render_template('character.html', **data['Characters'][character_id])
 
 #run Flask open to all IPs on port 80(requires root access)
 if __name__ == '__main__':
