7f780c3005befdb8f49f6ccdecf2c1ff14c81e5f
User <drconnoramerica@utexas.edu>
2015-03-27 00:58:21 +0000

    fixed db issue

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app = Flask(__name__)
-app.config['SQLACLHEMY_DATABASE_URI'] = 'postgresql://localhost/test'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/test'
 app.config['SQLALCHEMY_ECHO'] = False
 db = SQLAlchemy(app)
