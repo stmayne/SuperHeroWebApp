b2890611ab00b5c883133a623a89ef7799f6d1f4
User <drconnoramerica@utexas.edu>
2015-04-09 03:21:28 +0000

    No longer recreating db at beginging

--- app.py
+++ app.py
@@ -80,5 +80,5 @@ def getCharacterData(character_id=None):
 
 #run Flask open to all IPs on port 80(requires root access)
 if __name__ == '__main__':
-	create_models.run ()
+	#create_models.run ()
 	app.run(host='0.0.0.0', port=80, debug=True)
