33e54b6fc1ed6e8ca4959377bce66e032fa4940e
Oscar Muller <muller.oscar@gmail.com>
2015-04-23 18:57:09 -0500

    Testing auto log

--- checkfiles.bash
+++ checkfiles.bash
@@ -22,3 +22,27 @@ check_file "IDB.log"
 check_file "tests.py"
 check_file "tests.out"
 check_file "create_models.py"
+
+
+commit_message=`git log -1 --pretty=%B`
+commit_author=`git log -1 --pretty=%cn`
+commit_email=`git log -1 --pretty=%ce`
+
+git config --global user.name "$commit_author"
+git config --global user.email "$commit_email"
+git config --global push.default simple
+
+git config credential.helper "store --file=.git/credentials"
+echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+
+
+git branch travis-ci
+git checkout travis-ci
+
+
+git log > IDB.log
+git add -A
+git commit -m "Travis CI COmmit"
+git reset --soft HEAD~1
+git commit -m "$commit_message"
+git push -f "https://github.com/stmayne/cs373-idb.git" HEAD:travis-ci
